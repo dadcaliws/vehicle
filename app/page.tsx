444f9fc7ddf0a4bef1cf72ebd130afb05951dd34
dadcaliws <dadcaliws@gmail.com>
2024-10-08 00:19:11 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 import IntegratedSystem from '../components/integrated-system'
 
 export default function Home() {
-  return <IntegratedSystem />
+  return (
+    <main>
+      <IntegratedSystem />
+    </main>
+  )
 }
