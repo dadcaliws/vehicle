3722bcf1588dd5e6f874d8ac1fab4f39ccfb5fcd
dadcaliws <dadcaliws@gmail.com>
2024-10-08 00:58:10 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': path.resolve(__dirname, 'src'),
-    };
-    return config;
-  },
 }
 
 module.exports = nextConfig
